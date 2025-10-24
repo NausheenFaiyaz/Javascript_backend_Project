49852e7fc339eff3dfecf3a32f54151dedc486f4
Nausheen Faiyaz <logme2nausheen@gmail.com>
2025-10-24 21:30:06 +0530

    Connected MongoDB with Backend

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// require('dotenv').config({path: "./env"})
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error:", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed !!!", err);
+
+    throw err;
+  });
+
+/*
+import express from "express";
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.log("Error:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw error;
+  }
+})();
+*/
